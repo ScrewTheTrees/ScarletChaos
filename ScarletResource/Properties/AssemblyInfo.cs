211c6e55ed52e83e8d4addb7b93e65bd52b360ae
Fredrik Grimmenhag <fredrik@grimmenhag.se>
2017-10-31 11:28:04 +0100

    Updated some assembly info

--- ScarletResource/Properties/AssemblyInfo.cs
+++ ScarletResource/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ContentPipeline")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ScarletResource")]
+[assembly: AssemblyDescription("Primary game and editor DLL")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ContentPipeline")]
+[assembly: AssemblyProduct("ScarletResource")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
